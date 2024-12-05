a5dea018c8db6efdc18391317efbb55527935336
irkghub <bkbhandari99@gmail.com>
2024-12-05 17:13:07 +0545

    made changes to the project structure

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 import os
 import pandas as pd
 
-data = pd.read_csv("files/csv_file/weather_data.csv")
-
 current_dir = os.path.dirname(os.path.abspath(__file__))
+csv_file_path = os.path.join(current_dir,"files","csv_file","weather_data.csv")
 
-excel_path = os.path.join(current_dir,"files/excel_file","weather_data.xlsx")
+# Read CSV file into a dataframe now
+data = pd.read_csv(csv_file_path)
+excel_file_path = os.path.join(current_dir,"files","excel_file","weather_data.xlsx")
+data.to_excel(excel_file_path,"excel_file",index=False)
 
-data.to_excel(excel_path,"excel_file",index=False)
-
-print(f"Data has been successfully saved to {excel_path}")
+print(f"Data has been successfully saved to {excel_file_path}")
