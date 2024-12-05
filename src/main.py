9dd62a68e14902963933675c0b62471eacfd4b47
irkghub <bkbhandari99@gmail.com>
2024-12-05 17:19:58 +0545

    nothing to commit

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ csv_file_path = os.path.join(current_dir,"files","csv_file","weather_data.csv")
 
 # Read CSV file into a dataframe now
 data = pd.read_csv(csv_file_path)
-excel_file_path = os.path.join(current_dir,"files","excel_file","weather_data.xlsx")
+excel_file_path = os.path.join(current_dir,"files","excel_file","data.xlsx")
 data.to_excel(excel_file_path,"excel_file",index=False)
 
 print(f"Data has been successfully saved to {excel_file_path}")
